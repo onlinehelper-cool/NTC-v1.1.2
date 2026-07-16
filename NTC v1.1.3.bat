dd9f292f692e37d0b1f508ea540b6d6697cefcdc
Vtos1957 <wafflesguy12345@gmail.com>
2026-07-16 09:29:13 -0600

    Create NTC v1.1.3.bat

---
+++ NTC v1.1.3.bat
@@ -0,0 +1,54 @@
+@echo off
+title Network Traffic Capturing Batch Edition by @POt3tO (my youtube) v1.1.2
+echo =======================================================================
+echo UDP - 1
+echo TCP - 2
+echo TLS - 3
+echo stun - 4 [for online calls, snap, dischord, etc] (fixed)
+echo ALL - 5 (cluttered)
+echo =======================================================================
+echo enter the number corrisponded to the filter next to it.
+set /p mode="Enter mode: "
+if "%mode%"=="1" goto a
+if "%mode%"=="2" goto b
+if "%mode%"=="3" goto c
+if "%mode%"=="4" goto d
+if "%mode%"=="5" goto e
+:a
+cls
+pktmon stop
+pktmon filter remove
+cls
+pktmon filter add -t UDP
+pktmon start --etw -m real-time
+
+:b 
+cls
+pktmon stop
+pktmon filter remove
+cls
+pktmon filter add -t TCP
+pktmon start --etw -m real-time
+
+:c
+cls
+pktmon stop
+pktmon filter remove
+cls
+pktmon filter add -p 443
+pktmon start --etw -m real-time
+
+:d
+cls
+pktmon stop
+pktmon filter remove
+cls
+pktnom filter add -t UDP
+pktmon start --etw -m real-time
+
+:e
+cls 
+pktmon stop
+pktmon filter remove
+cls
+pktmon start --etw -m real-time
